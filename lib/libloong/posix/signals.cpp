a02b423518f527b4e3a0a054b8364728171e882b
Alf-André Walla <fwsgonzo@hotmail.com>
2025-12-09 15:51:29 +0100

    Clean up minimal signal handling

--- lib/libloong/posix/signals.cpp
+++ lib/libloong/posix/signals.cpp
@@ -6,11 +6,14 @@ namespace loongarch
 {
 	SignalAction& Signals::get(int sig)
 	{
-		if (sig < 1 || sig > 64) {
+		if (sig == 0) {
+			return signals[0];
+		}
+		else if (sig < 1 || sig > 64) {
 			throw MachineException(INVALID_PROGRAM,
 				"Signal number out of range", sig);
 		}
-		return signals[sig-1];
+		return signals.at(sig-1);
 	}
 
 	void Signals::enter(Machine& machine, int sig)
@@ -19,17 +22,15 @@ namespace loongarch
 		if (sig == 0)
 			return;
 
-		auto& sigact = signals.at(sig);
+		auto& sigact = this->get(sig);
 
 		// Switch to alternate stack if configured
 		if (sigact.altstack) {
-			printf("Switching to alternate signal stack from 0x%lx for signal %d\n", machine.cpu.reg(REG_SP), sig);
 			auto& stack = per_thread(machine.gettid()).stack;
 			machine.cpu.reg(REG_SP) = stack.ss_sp + stack.ss_size;
 		}
 
 		// Jump to handler
-		printf("Jumping to signal handler 0x%lx for signal %d (stack=0x%lx)\n", sigact.handler, sig, machine.cpu.reg(REG_SP));
 		machine.cpu.jump(sigact.handler-4);
 	}
 

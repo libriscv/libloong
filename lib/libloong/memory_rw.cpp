54aceff3f68659cd8b9b7b37f042d3f85815e23d
Alf-André Walla <fwsgonzo@hotmail.com>
2025-12-09 15:51:52 +0100

    Add unused masking for memory helpers

--- lib/libloong/memory_rw.cpp
+++ lib/libloong/memory_rw.cpp
@@ -1,11 +1,21 @@
 #include "memory.hpp"
 #include "machine.hpp"
+#define OVER_ALLOCATE_SIZE 64 /* Avoid SIMD bounds-check */
 
 namespace loongarch
 {
 	void Memory::copy_to_guest(address_t dest, const void* src, size_t len)
 	{
-		if (LA_UNLIKELY(!is_writable(dest, len))) {
+		if constexpr (false && LA_MASKED_MEMORY_MASK) {
+			if constexpr (LA_MASKED_MEMORY_MASK == UINT32_MAX) {
+				dest = (uint32_t)dest;
+			} else {
+				dest &= LA_MASKED_MEMORY_MASK;
+			}
+			if (LA_UNLIKELY(dest + len >= LA_MASKED_MEMORY_SIZE)) {
+				throw MachineException(PROTECTION_FAULT, "Write to out-of-bounds memory", dest);
+			}
+		} else if (LA_UNLIKELY(!is_writable(dest, len))) {
 			throw MachineException(PROTECTION_FAULT, "Write to read-only memory", dest);
 		}
 
@@ -14,20 +24,29 @@ namespace loongarch
 
 	void Memory::copy_from_guest(void* dest, address_t src, size_t len) const
 	{
-		if (LA_UNLIKELY(src < m_rodata_start || src + len >= m_arena_size)) {
+		if constexpr (false && LA_MASKED_MEMORY_MASK) {
+			if constexpr (LA_MASKED_MEMORY_MASK == UINT32_MAX) {
+				src = (uint32_t)src;
+			} else {
+				src &= LA_MASKED_MEMORY_MASK;
+			}
+			if (LA_UNLIKELY(src + len >= LA_MASKED_MEMORY_SIZE)) {
+				throw MachineException(PROTECTION_FAULT, "Read from out-of-bounds memory", src);
+			}
+		} else if (LA_UNLIKELY(src < m_rodata_start || src + len >= m_arena_size)) {
 			throw MachineException(PROTECTION_FAULT, "Read from unmapped memory", src);
 		}
 
 		std::memcpy(dest, &m_arena[src], len);
 	}
 
-	void Memory::memset(address_t addr, uint8_t value, size_t len)
+	void Memory::memset(address_t dest, uint8_t value, size_t len)
 	{
-		if (LA_UNLIKELY(!is_writable(addr, len))) {
-			throw MachineException(PROTECTION_FAULT, "Write to read-only memory", addr);
+		if (LA_UNLIKELY(!is_writable(dest, len))) {
+			throw MachineException(PROTECTION_FAULT, "Write to read-only memory", dest);
 		}
 
-		std::memset(&m_arena[addr], value, len);
+		std::memset(&m_arena[dest], value, len);
 	}
 
 	int Memory::memcmp(address_t addr1, address_t addr2, size_t len) const
